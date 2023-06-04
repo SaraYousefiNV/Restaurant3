289c7c5624e7df8c540944fd2fc3efba2fab4b97
SaraYousefiNV <sarayousefi_krc@yahoo.com>
2023-06-04 18:42:43 +0330

    sa

---
+++ Services/OrderType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public enum OrderType
+    {
+     getOut=1,
+     getIn=2
+    }
+}
