683333925e87d2b41400116fb12d91002af47ce9
SaraYousefiNV <sarayousefi_krc@yahoo.com>
2023-06-04 19:18:47 +0330

    ااااااس

--- Restaurant3/FrmAdmin.Designer.cs
+++ Restaurant3/FrmAdmin.Designer.cs
@@ -39,22 +39,27 @@
             this.txtName = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label4.Location = new System.Drawing.Point(475, 180);
+            this.label4.Location = new System.Drawing.Point(470, 133);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(43, 13);
             this.label4.TabIndex = 18;
-            this.label4.Text = ": موبایل";
+            this.label4.Text = "موبایل :";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // txtAddress
             // 
-            this.txtAddress.Location = new System.Drawing.Point(279, 177);
+            this.txtAddress.Location = new System.Drawing.Point(274, 130);
             this.txtAddress.Name = "txtAddress";
             this.txtAddress.Size = new System.Drawing.Size(181, 20);
             this.txtAddress.TabIndex = 17;
@@ -63,33 +68,33 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label3.Location = new System.Drawing.Point(477, 144);
+            this.label3.Location = new System.Drawing.Point(472, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.Size = new System.Drawing.Size(77, 13);
             this.label3.TabIndex = 16;
-            this.label3.Text = ": کد ملی";
+            this.label3.Text = "ساعت کارکرد :";
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(279, 137);
+            this.txtID.Location = new System.Drawing.Point(374, 97);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(181, 20);
+            this.txtID.Size = new System.Drawing.Size(70, 20);
             this.txtID.TabIndex = 15;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label2.Location = new System.Drawing.Point(477, 109);
+            this.label2.Location = new System.Drawing.Point(472, 62);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.Size = new System.Drawing.Size(99, 13);
             this.label2.TabIndex = 14;
-            this.label2.Text = ": نام خانوادگی";
+            this.label2.Text = "نام صاحب رستوران :";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // txtFamily
             // 
-            this.txtFamily.Location = new System.Drawing.Point(279, 102);
+            this.txtFamily.Location = new System.Drawing.Point(274, 55);
             this.txtFamily.Name = "txtFamily";
             this.txtFamily.Size = new System.Drawing.Size(181, 20);
             this.txtFamily.TabIndex = 13;
@@ -98,15 +103,15 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label1.Location = new System.Drawing.Point(477, 70);
+            this.label1.Location = new System.Drawing.Point(472, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.Size = new System.Drawing.Size(68, 13);
             this.label1.TabIndex = 12;
-            this.label1.Text = ": نام";
+            this.label1.Text = "نام رستوران :";
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(317, 255);
+            this.btnSave.Location = new System.Drawing.Point(312, 208);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(83, 39);
             this.btnSave.TabIndex = 11;
@@ -115,14 +120,14 @@
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(279, 67);
+            this.txtName.Location = new System.Drawing.Point(274, 20);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(181, 20);
             this.txtName.TabIndex = 10;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(279, 210);
+            this.textBox1.Location = new System.Drawing.Point(274, 163);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(181, 20);
             this.textBox1.TabIndex = 17;
@@ -131,19 +136,56 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.label5.Location = new System.Drawing.Point(475, 217);
+            this.label5.Location = new System.Drawing.Point(470, 170);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(36, 13);
             this.label5.TabIndex = 18;
-            this.label5.Text = ": آدرس";
+            this.label5.Text = "آدرس :";
             this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(274, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 20);
+            this.textBox2.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(450, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "از :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(347, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "تا :";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(94, 274);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(573, 150);
+            this.dgv.TabIndex = 22;
             // 
             // FrmAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
@@ -156,9 +198,11 @@
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.txtName);
             this.Name = "FrmAdmin";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ثبت نام ادمین";
+            this.Text = "ثبت نام رستوران";
             this.Load += new System.EventHandler(this.FrmAdmin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,5 +221,9 @@
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dgv;
     }
 }
