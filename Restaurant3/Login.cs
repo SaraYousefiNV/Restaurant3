af7e139095202b969a12621f630d1c25d529c0ef
Naeim Fatemi <134975725+Naeimfatemi@users.noreply.github.com>
2023-06-10 02:12:21 +0330

    تفکیک فرم لاگین بر اساس تایپ انجام شد. درصورتی که تایپ 1 باشد فرم رستوران و در صورتی که تایپ 2 باشد فرم منو باز میشه .

--- Restaurant3/Login.cs
+++ Restaurant3/Login.cs
@@ -24,21 +24,53 @@ namespace Restaurant3
 
         private void btnSignIn_Click(object sender, EventArgs e)
         {
-            SqlDataAdapter da = new SqlDataAdapter("select count(*) from Users where UserName='" + txtUser.Text + "' and Password='" + txtPassword.Text + "'", Constanst.ConnectionString);
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT COUNT(*) FROM Users WHERE UserName='" + txtUser.Text + "' and Password='" + txtPassword.Text + "'", Constanst.ConnectionString);
             DataTable dt = new DataTable();
             da.Fill(dt);
-            if (dt.Rows[0][0].ToString() == "1")
+            if (dt.Rows[0][0].ToString() == "1" && GetSelectedUserType(txtUser.Text) == "2")
             {
                 MessageBox.Show("خوش آمدید");
-
                 MenuForm menuForm = new MenuForm();
                 menuForm.ShowDialog();
+            }
+            else if (dt.Rows[0][0].ToString() == "1" && GetSelectedUserType(txtUser.Text) == "1")
+            {
+                MessageBox.Show("خوش آمدید");
+                RestaurantForm restaurantForm = new RestaurantForm();
+                restaurantForm.ShowDialog();
             }
             else
             {
 
                 MessageBox.Show(".نام کاربری یا پسورد اشتباه است");
             }
+        }
+        private string GetSelectedUserType(string userName)
+        {
+            string userType = string.Empty;
+
+            string query = "SELECT Type FROM [Users] WHERE UserName = @UserName";
+
+            using (SqlConnection connection = new SqlConnection(Constanst.ConnectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserName", userName);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            userType = reader["Type"].ToString();
+                        }
+                    }
+                }
+            }
+
+            return userType;
         }
     }
 }
